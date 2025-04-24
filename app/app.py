5d6afd6c70cc9e5399ca7cf9b7f4bc1ad3673337
Asim <asimmajeed202@gmail.com>
2025-04-24 20:40:13 +0500

    Implement user authentication with login

--- app/app.py
+++ app/app.py
@@ -1,18 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import client  # uses your SSL socket client code
 
 app = Flask(__name__)
+app.secret_key = 'devsecretkey'  # used for session cookies
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Simulated User Class
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+# Load user from session
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 @app.route('/')
-def home():
-    return "Welcome to the Authentication System"
+def index():
+    return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        response = client.send_request_secure('login', username, password)
+        if "successful" in response:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash("Login failed!")
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        response = client.send_request_secure('register', username, password)
+        flash(response)
     return render_template('register.html')
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html', name=current_user.id)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
